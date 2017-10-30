d8b883a681033090b0ff72f051e749753ac2e14a
catalincostea <catalin.costea@gmail.com>
2017-10-30 17:10:19 +0200

    alb

--- examples/alb/main.tf
+++ examples/alb/main.tf
@@ -1,30 +1,11 @@
-# Specify the provider and access details
-provider "aws" {
-  region = "${var.aws_region}"
-}
-
-resource "aws_instance" "web" {
-  instance_type = "t2.micro"
-
-  # Lookup the correct AMI based on the region
-  # we specified
-  ami = "ami-5f140c39"
-
-  # The name of our SSH keypair you've created and downloaded
-  # from the AWS console.
-  #
-  # https://console.aws.amazon.com/ec2/v2/home?region=us-west-2#KeyPairs:
-  #
-  key_name = "test"
-
-  # Our Security group to allow HTTP and SSH access
-  vpc_security_group_ids = ["sg-c2d5d5ba"]
-  subnet_id              = "subnet-49850c3e"
-  user_data              = "${file("userdata.sh")}"
-
-  #Instance tags
-
-  tags {
-    Name = "elb-example"
+resource "aws_alb" "alb" {
+  name            = "${var.name}-alb-${var.environment}"
+  internal        = true
+  security_groups = ["${var.security_group_id}"]
+  subnets         = ["${split(",", var.subnet_ids)}"]
+tags {
+    Environment = "${var.environment}"
   }
 }
+
+#https://hackernoon.com/configuring-the-new-alb-host-based-routing-with-terraform-358361bad12b
